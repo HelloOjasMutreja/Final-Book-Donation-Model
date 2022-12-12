9bda47591665e2fff40e1e91268d28312940593c
Ojas Mutreja <helloojasmutreja@gmail.com>
2022-12-13 00:36:25 +0530

    Styling Added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!, only: %i[ new, edit ]
+  before_action :authenticate_user!, only: %i[ new edit ]
   before_action :set_categories
 
   # GET /books or /books.json
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,19 @@ import "controllers"
 
 //= require jquery
 //= require bootstrap-sprockets
+
+(function () {
+    'use strict'
+    const forms = document.querySelectorAll('.requires-validation')
+    Array.from(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+    
+          form.classList.add('was-validated')
+        }, false)
+      })
+    })()
